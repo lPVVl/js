725c355ebe2ed6aa3d20b698319d8ff9becccf27
vlad <fff@gmail.com>
2022-04-19 19:55:00 +0300

    test

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/* const result = confirm('are you here?');
+console.log(result); */
+
+
+/* const answers = [];
+
+answers[0] = prompt('kak vashe name', '');
+answers[1] = prompt('kak vashe familia', '');
+answers[2] = prompt('skolko tebe let bichara', '');
+
+console.log(typeof(null)); */
+
+console.log(2*2 === '4');

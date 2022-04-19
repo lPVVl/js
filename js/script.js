cee70176b8e4f44901c599e67d2ed35af1acd169
vlad <fff@gmail.com>
2022-04-19 20:48:53 +0300

    выполнил задание 3

--- js/script.js
+++ js/script.js
@@ -22,9 +22,19 @@ console.log(isCheked && isClose); */
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
 
 const personalMovieDB = {
-    count: numberOfFilms.toExponential,
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?'),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);

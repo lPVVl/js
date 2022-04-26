b6126a8530b947bfed3c739c5bbe07ec6198ef21
vlad <fff@gmail.com>
2022-04-26 11:56:55 +0300

    Метки

--- js/script.js
+++ js/script.js
@@ -1,40 +1,22 @@
 "use strict";
 
-/* const result = confirm('are you here?');
-console.log(result); */
+let num = 50;
 
+while (num <= 55) {
+    console.log(num);
+    num++;
+}
 
-/* const answers = [];
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
 
-answers[0] = prompt('kak vashe name', '');
-answers[1] = prompt('kak vashe familia', '');
-answers[2] = prompt('skolko tebe let bichara', '');
-
-console.log(typeof(null)); */
-
-/* console.log(2*2 === '4');
-
-const isCheked = true,
-      isClose = true;
-
-console.log(isCheked && isClose); */
-
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?'),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+       // break;
+       continue;
+    }
+    console.log(i);
+}

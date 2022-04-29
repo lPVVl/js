0e5fb3a6557e16ef646947861c5cfb6cc41b8db6
PetrichicVV <101114878+lPVVl@users.noreply.github.com>
2022-04-29 14:52:13 +0300

    Add files via upload

---
+++ string number/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+
+const str = 'test'; //4 console.log(str.length);
+const arr = [1, 2 ,4]; //3 console.log(arr.length);
+
+console.log(arr.length);
+console.log(str.toUpperCase()); // все прописные
+console.log(str.toLowerCase()); // все строчне
+
+const fruit = 'some fruit';
+
+console.log(fruit.indexOf('fruit')); // поиск подстроки
+
+const logg = "hello world";
+
+console.log(logg.slice(6, 11)); //вырезка
+console.log(logg.substring(6, 11));
+
+const num = 12.2;
+console.log(Math.round(num)); // округление чисел
+
+const test = '12.2px';
+console.log(parseInt(test)); // округление в числовой тип данных
+console.log(parseFloat(test)); // возвращает с дробными ззначениями
+
+
+
